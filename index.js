32622ffefc1e3c811aa0b73ff2fe56a8f0a75f7f
jfbz-epam <juan_bustamante@epam.com>
2022-04-18 16:35:30 -0500

    added filename from CLI

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const validators = require('./src/validators')
 const fs = require('fs');
 
 let myStructure = folder('root');
-
-const read = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+let fileName;
 
 /**
  * performs request of the CLI
@@ -23,9 +19,23 @@ function doRequest(params) {
   }
 }
 
+/**
+ * reads 3rd argument from CLI. if empty message "filename cannot be empty" will
+ * be displayed. otherwise it will read  file line by line. 
+ *
+ */
+try {
+  fileName=process.argv[2];
+  const allFileContents = fs.readFileSync(fileName, 'utf-8');
+  allFileContents.split(/\r?\n/).forEach(line =>  {
+    doRequest(line);
+  });
+  process.exit(0);
+} catch (error) {
+  console.log(`filename cannot be empty`);
+  process.exit(1);
+}
 
 
-const allFileContents = fs.readFileSync('script.txt', 'utf-8');
-allFileContents.split(/\r?\n/).forEach(line =>  {
-  doRequest(line);
-});
+
+
